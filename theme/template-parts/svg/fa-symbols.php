916bbe8a55c1d46659a55f632131017b5a19d3ca
Daniel Bishop <dbishop@beachfleischman.com>
2022-11-30 12:55:27 -0700

    Adding more fa svg support

--- theme/template-parts/svg/fa-symbols.php
+++ theme/template-parts/svg/fa-symbols.php
@@ -5,15 +5,30 @@
  * see: https://fontawesome.com/docs/web/add-icons/svg-symbols
  */
 ?>
-<!-- Name symbols with the value of data-fa-symbol -->
+
+<?php // Name symbols with the value of data-fa-symbol ?>
 <i data-fa-symbol="bars" class="fa-regular fa-bars"></i>
+<i data-fa-symbol="caret-up" class="fa-regular fa-caret-up"></i>
+<i data-fa-symbol="caret-right" class="fa-regular fa-caret-right"></i>
 <i data-fa-symbol="caret-down" class="fa-regular fa-caret-down"></i>
+<i data-fa-symbol="caret-left" class="fa-regular fa-caret-left"></i>
+<i data-fa-symbol="angle-up" class="fa-regular fa-angle-up"></i>
+<i data-fa-symbol="angle-right" class="fa-regular fa-angle-right"></i>
 <i data-fa-symbol="angle-down" class="fa-regular fa-angle-down"></i>
+<i data-fa-symbol="angle-left" class="fa-regular fa-angle-left"></i>
+<i data-fa-symbol="arrow-up" class="fa-regular fa-arrow-up"></i>
 <i data-fa-symbol="arrow-right" class="fa-regular fa-arrow-right"></i>
+<i data-fa-symbol="arrow-down" class="fa-regular fa-arrow-down"></i>
+<i data-fa-symbol="arrow-left" class="fa-regular fa-arrow-left"></i>
+<i data-fa-symbol="people-group" class="fa-solid fa-people-group"></i>
+<i data-fa-symbol="location-dot" class="fa-solid fa-location-dot"></i>
 <i data-fa-symbol="linkedin" class="fa-brands fa-linkedin"></i>
 <i data-fa-symbol="twitter" class="fa-brands fa-twitter"></i>
 <i data-fa-symbol="facebook" class="fa-brands fa-facebook"></i>
 <i data-fa-symbol="instagram" class="fa-brands fa-instagram"></i>
 <i data-fa-symbol="youtube" class="fa-brands fa-youtube"></i>
-<!-- Use the defined name later in the page -->
-<!-- <svg><use xlink:href="#bars"></use> -->
+<?php
+/**
+ * Use the defined name later in the page
+ * <svg><use xlink:href="#bars"></use></svg>
+ */
