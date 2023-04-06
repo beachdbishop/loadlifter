265806c1f4ecc462f424a705c119c262c4720667
Daniel Bishop <dbishop@beachfleischman.com>
2023-04-06 14:31:13 -0700

    Making it easier to select trending items on front page

--- theme/template-parts/content/content-page-careers.php
+++ theme/template-parts/content/content-page-careers.php
@@ -7,26 +7,6 @@
  * @package Load_Lifter
  */
 
-$gradient = 'linear-gradient(to right, rgba(9,47,66,1) 0%, rgba(9,47,66,0.9) 25%, rgba(9,47,66,0.8) 40%, rgba(9,47,66,0.6) 55%, rgba(9,47,66,0.3) 70%, rgba(9,47,66,0.2) 80%, rgba(9,47,66,0.1) 90%, rgba(9,47,66,0) 100%)';
-$easedGradient = 'linear-gradient(
-    to right,
-    hsla(0, 0%, 0%, 0.97) 0%,
-    hsla(210, 50%, 0.78%, 0.959) 8.1%,
-    hsla(210, 66.67%, 2.35%, 0.928) 15.5%,
-    hsla(206.25, 61.54%, 5.1%, 0.88) 22.5%,
-    hsla(206.67, 62.79%, 8.43%, 0.817) 29%,
-    hsla(207, 62.5%, 12.55%, 0.744) 35.3%,
-    hsla(207.78, 61.36%, 17.25%, 0.664) 41.2%,
-    hsla(207.43, 62.5%, 21.96%, 0.578) 47.1%,
-    hsla(208.24, 62.04%, 26.86%, 0.492) 52.9%,
-    hsla(207.92, 62.73%, 31.57%, 0.406) 58.8%,
-    hsla(208.17, 62.16%, 36.27%, 0.326) 64.7%,
-    hsla(208.13, 62.14%, 40.39%, 0.253) 71%,
-    hsla(208.06, 62.33%, 43.73%, 0.19) 77.5%,
-    hsla(207.76, 62.03%, 46.47%, 0.142) 84.5%,
-    hsla(207.84, 62.45%, 48.04%, 0.111) 91.9%,
-    hsla(207.87, 62.25%, 48.82%, 0.1) 100%
-  )';
 $page_id = get_the_ID();
 if (get_field('ll_page_title_override')) {
     $page_title = get_field('ll_page_title_override');
@@ -102,28 +82,7 @@ if ('local' === wp_get_environment_type()) {
 }
 ?>
 
-<style>
-<?php if ($page_icon) {
-    echo ':root { --page-icon-class: ' . $page_icon . ' }';
-} ?><?php // We're setting inline styles here because we need to include the responsive gradient AND dynamic image URL in the same background-image declaration;
-?>.page-hero { background-image: <?php echo $gradient; ?>, url('<?php echo esc_url($page_featimg_url); ?>'); }
-@media (min-width: 768px) { .page-hero { background-image: <?php echo $easedGradient; ?>, url('<?php echo esc_url($page_featimg_url); ?>'); } }
-</style>
-
-<header class="page-hero | ll-equal-vert-padding bg-brand-blue-dark bg-no-repeat bg-[right_33%_center] bg-cover lg:bg-center print:py-8" itemprop="image" itemscope itemtype="https://schema.org/ImageObject" aria-label="<?php the_title_attribute(); ?>">
-    <div class="flex flex-col justify-center px-2 min-h-[240px] md:container md:mx-auto md:px-0 md:min-h-hero">
-
-        <div class="">
-            <h1 class="leading-none text-white tracking-light text-shadow-lg shadow-neutral-900 lg:text-6xl"><?php echo $page_title; ?></h1>
-            <h2 class="mt-4 text-2xl leading-normal text-brand-blue-pale text-shadow-lg shadow-neutral-900 lg:text-4xl"><?php echo $page_excerpt; ?></h2>
-        </div>
-
-    </div>
-    <?php if (function_exists('bcn_display') && !is_front_page()) { ?>
-        <div class="breadcrumbs | container mx-auto px-2 md:px-0 font-head text-brand-gray-faint" typeof="BreadcrumbList" vocab="https://schema.org"><?php bcn_display(); ?></div>
-    <?php } ?>
-</header>
-
+<?php ll_page_hero( $page_title, $page_excerpt, $page_featimg_url ); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
     <div class="px-1 md:container md:mx-auto md:px-0">
--- theme/template-parts/content/content-page-industry.php
+++ theme/template-parts/content/content-page-industry.php
@@ -7,31 +7,15 @@
  * @package Load_Lifter
  */
 
-$gradient = 'linear-gradient(to right, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0.9) 60%, rgba(0,0,0,0.7) 75%, rgba(0,0,0,0.5) 90%, rgba(0,0,0,0.2) 100%)';
-$easedGradient = 'linear-gradient(
-	  to right,
-	  hsla(0, 0%, 0%, 0.97) 0%,
-	  hsla(210, 50%, 0.78%, 0.959) 8.1%,
-	  hsla(210, 66.67%, 2.35%, 0.928) 15.5%,
-	  hsla(206.25, 61.54%, 5.1%, 0.88) 22.5%,
-	  hsla(206.67, 62.79%, 8.43%, 0.817) 29%,
-	  hsla(207, 62.5%, 12.55%, 0.744) 35.3%,
-	  hsla(207.78, 61.36%, 17.25%, 0.664) 41.2%,
-	  hsla(207.43, 62.5%, 21.96%, 0.578) 47.1%,
-	  hsla(208.24, 62.04%, 26.86%, 0.492) 52.9%,
-	  hsla(207.92, 62.73%, 31.57%, 0.406) 58.8%,
-	  hsla(208.17, 62.16%, 36.27%, 0.326) 64.7%,
-	  hsla(208.13, 62.14%, 40.39%, 0.253) 71%,
-	  hsla(208.06, 62.33%, 43.73%, 0.19) 77.5%,
-	  hsla(207.76, 62.03%, 46.47%, 0.142) 84.5%,
-	  hsla(207.84, 62.45%, 48.04%, 0.111) 91.9%,
-	  hsla(207.87, 62.25%, 48.82%, 0.1) 100%
-	)';
 $ind_id = get_the_ID();
-
+if (get_field('ll_page_title_override')) {
+    $page_title = get_field('ll_page_title_override');
+} else {
+    $page_title = get_the_title();
+}
 $ind_icon = ( get_field( 'll_page_icon' ) ) ? get_field( 'll_page_icon' ) : false;
 $ind_message = get_field( 'll_brand_message' );
-// $ind_excerpt = get_the_excerpt();
+$ind_excerpt = get_the_excerpt();
 $ind_featimg = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
 if ( $ind_featimg == true ) {
 	$ind_featimg_url = $ind_featimg[0];
@@ -44,28 +28,9 @@ $ind_cta_html = get_field( 'll_ind_cta_html' );
 $ind_groups_html = get_field( 'll_ind_groups_html' );
 $ind_people = get_field( 'll_ind_people' );
 $ind_people_display = get_field( 'll_ind_people_display_style' );
-// if ( $ind_people_display === 'slider' ) {
-//     add_action( 'wp_enqueue_scripts', 'll_forceenq_a11y_slider_assets' );
-// }
 ?>
 
-<style><?php // We're setting inline styles here because we need to include the responsive gradient AND dynamic image URL in the same background-image declaration; ?>.page-hero { background-image: <?php echo $gradient; ?>, url('<?php echo esc_url( $ind_featimg_url ); ?>'); }
-@media (min-width: 768px) { .page-hero { background-image: <?php echo $easedGradient; ?>, url('<?php echo esc_url( $ind_featimg_url ); ?>'); } }</style>
-
-<header class="page-hero | ll-equal-vert-padding bg-brand-blue-dark bg-no-repeat bg-[right_33%_center] bg-cover lg:bg-center print:py-8" itemprop="image" itemscope itemtype="https://schema.org/ImageObject" aria-label="<?php the_title_attribute(); ?>">
-	<div class="flex flex-col justify-center px-2 min-h-[240px] md:container md:mx-auto md:px-0 md:min-h-hero">
-
-		<div class="">
-            <h1 class="leading-none text-white tracking-light text-shadow-lg shadow-neutral-900 lg:text-6xl"><?php echo get_the_title(); ?></h1>
-			<h2 class="mt-4 text-2xl leading-normal text-brand-blue-pale text-shadow-lg shadow-neutral-900 lg:text-4xl"><?php echo $ind_message['label']; ?></h2>
-			<!-- <p class="mt-4 leading-normal text-white lg:text-lg"><?php // echo $ind_excerpt; ?></p> -->
-		</div>
-
-	</div>
-    <?php if (function_exists('bcn_display') && !is_front_page()) { ?>
-        <div class="breadcrumbs | container mx-auto px-2 md:px-0 font-head text-brand-gray-faint" typeof="BreadcrumbList" vocab="https://schema.org"><?php bcn_display(); ?></div>
-    <?php } ?>
-</header>
+<?php ll_page_hero( $page_title, $ind_message['label'], $ind_featimg_url ); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="px-2 md:container md:mx-auto md:px-0">
