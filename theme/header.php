1573581e04a9aab2c2ae3c4c36d9b344ea931d01
Daniel Bishop <dbishop@beachfleischman.com>
2023-04-18 12:52:22 -0700

    Culling unused header and footer partials

--- theme/header.php
+++ theme/header.php
@@ -37,7 +37,7 @@
 	<a href="#primary" class="screen-reader-text"><?php esc_html_e( 'Skip to content', 'loadlifter' ); ?></a>
 
 	<?php if ( ( !is_page_template( 'tpl-landing-page.php' ) ) && ( !is_page_template( 'tpl-landing-page-bare.php' ) ) ) {
-        get_template_part( 'template-parts/layout/header-lessleaf', 'content');
+        get_template_part( 'template-parts/layout/header', 'content');
 	} else {
 		get_template_part( 'template-parts/layout/header', 'lp' );
 	} ?>
