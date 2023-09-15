50320fb3e57d77bca287f99491c8b16bfdd7845d
Daniel Bishop <dbishop@beachfleischman.com>
2023-09-15 10:12:41 -0700

    Adding some experimental red and blue colors

--- theme/page-client-center.php
+++ theme/page-client-center.php
@@ -13,6 +13,19 @@
  * @package Load_Lifter
  */
 
+if ( get_field( 'll_page_title_override' ) ) {
+	$page_title = get_field( 'll_page_title_override' );
+} else {
+	$page_title = get_the_title();
+}
+$page_message = get_field( 'll_brand_message' );
+$page_featimg = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+if ( $page_featimg == true ) {
+	$page_featimg_url = $page_featimg[0];
+} else {
+	$page_featimg_url = '';
+}
+
 get_header();
 ?>
 
@@ -24,18 +37,25 @@ get_header();
 		// get_template_part( 'template-parts/content/content', 'page' );
 	?>
 
+    <?php ll_page_hero( $page_title, $page_message['label'], $page_featimg_url ); ?>
+
 		<article id="post-<?php the_ID(); ?>" <?php if (!is_front_page()) { post_class('py-4 lg:py-8'); } ?>>
 			<div class="px-1 md:container md:mx-auto md:px-0">
-				<?php if (function_exists('bcn_display')) { ?>
-					<div class="breadcrumbs | font-head text-neutral-700 pb-4 md:pb-6 lg:pb-8" typeof="BreadcrumbList" vocab="https://schema.org"><?php bcn_display(); ?></div>
-				<?php } ?>
-
-				<header>
-					<?php the_title('<h1 class="entry-title | ">', '</h1>'); ?>
-				</header>
 
 				<div class="not-prose entry-cont">
 					<?php // the_content(); ?>
+
+                    <div class="prose">
+                        <h2 class="todo">Elements to include on page:</h2>
+                        <ul class="mb-24 todo">
+                            <li>AuditDashboard</li>
+                            <li>ShareFile</li>
+                            <li>TaxCaddy</li>
+                            <li>SafeSend Returns</li>
+                            <li>Payment Options (Invoice + Deposit)</li>
+                            <li>Payment Help info</li>
+                        </ul>
+                    </div>
 
 					<section class="mb-8 lg:mb-16">
                         <div class="p-4 mb-10 border-2 border-solid divide-y rounded-lg border-neutral-200 bg-neutral-50 md:mb-0 divide-neutral-200 divide-solid lg:p-8 hover:bg-white">
