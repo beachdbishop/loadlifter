37b1971f33aa86a9b49b20288acd8efcb4986015
Daniel Bishop <dbishop@beachfleischman.com>
2023-10-26 13:13:08 -0700

    Adding a demo announcment banner

--- theme/template-parts/siteblocks/pre-header.php
+++ theme/template-parts/siteblocks/pre-header.php
@@ -3,10 +3,14 @@
  * Pre-Header
  */
 ?>
-<?php /*
-<section class="bg-yellow-300 full-bleed text-neutral-900">
-	<div class="container py-2 mx-auto">
-		<p>Pre-Header</p>
+<section class="bg-gradient-to-r from-indigo-600 from-10% via-sky-500 via-30% to-emerald-600 to-90% full-bleed">
+	<div class="container py-2 mx-auto md:py-4">
+		<p class="text-lg text-center text-neutral-100 animate-fade-in">
+            <i class="fa-duotone fa-megaphone"></i>
+            <strong>
+                Important Announcement:
+            </strong>
+            <span class="hidden md:inline">Something something podiatrist sandwich container </span><a href="#" class="underline underline-offset-2 decoration-indigo-300 hover:underline hover:text-white hover:decoration-indigo-500 dark:decoration-indigo-600">Lunch Bag Horseshoe on November 1, 2023</a>.
+        </p>
 	</div>
 </section>
-*/ ?>
