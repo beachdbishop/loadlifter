f19d1a552b45f4c3c59fe3dfbdd68a3de8e2b9ef
Daniel Bishop <dbishop@beachfleischman.com>
2023-11-15 16:12:15 -0700

    Adding noscript options for form embeds
    A user can still email us even if they have JS turned off.

--- theme/template-parts/form/form-hubspot-contact-sidebar.php
+++ theme/template-parts/form/form-hubspot-contact-sidebar.php
@@ -1,11 +1,17 @@
 <?php
 // intended partial to be used in sidebar of pages / posts
 if ( ( in_category( 'construction' ) ) || ( is_page( 'Construction' ) ) ) {
-    $hs_form_id = 'fd7acd56-f9ce-4b38-863b-20cb2530a493';
+		$hs_form_id = 'fd7acd56-f9ce-4b38-863b-20cb2530a493';
 } else {
-    $hs_form_id = 'c8675641-3e68-4ff7-9dc3-ae3636fbf1c8';
+		$hs_form_id = 'c8675641-3e68-4ff7-9dc3-ae3636fbf1c8';
 }
 // echo ( wp_get_environment_type() == 'local' ) ? '<p class="italic">partial: ' . __FILE__ . '</p>' : '';
 ?>
 <h3 class="mb-4 text-brand-blue-dark dark:text-brand-blue-pale print:hidden">Contact us</h3>
 <script>hbspt.forms.create({region: "na1", portalId: "5578910", formId: "<?php echo $hs_form_id; ?>"});</script>
+<noscript>
+	<p class="my-4">Let us know what you need.</p>
+	<div class="wp-block-buttons is-content-justification-left is-layout-flex wp-block-buttons-is-layout-flex print:hidden">
+		<div class="wp-block-button is-style-outline"><a href="mailto:info@beachfleischman.com?subject=Inquiry%20from:%20<?php echo esc_attr( get_the_title() ); ?>" class="wp-block-button__link has-brand-red-color has-text-color wp-element-button "><i class="fa-solid fa-envelope"></i> Email us</a></div>
+	</div>
+</noscript>
--- theme/template-parts/form/form-hubspot-newsletter-onlight.php
+++ theme/template-parts/form/form-hubspot-newsletter-onlight.php
@@ -2,12 +2,15 @@
 // form - HubSpot - Newsletter Subscription (on light bg)
 ?>
 <div class="p-4 md:p-8 bg-neutral-50 text-neutral-800 sticky z-10 top-[104px] print:hidden">
-
-    <?php // echo ( wp_get_environment_type() == 'local' ) ? '<p class="italic">form-hubspot-newsletter-onlight.php</p>' : ''; ?>
-
-    <h3 class="mb-4 text-brand-red">Stay up-to-date</h3>
+	<?php // echo ( wp_get_environment_type() == 'local' ) ? '<p class="italic">form-hubspot-newsletter-onlight.php</p>' : ''; ?>
+	<h3 class="mb-4 text-brand-red">Stay up-to-date</h3>
 	<p class="mb-8">Get a collection of our latest blog posts in your inbox every 3-4 weeks.</p>
 	<script>
 		hbspt.forms.create({region: "na1", portalId: "5578910", formId: "e9f9a025-96c0-4a18-b642-17a605422edf"});
 	</script>
+	<noscript>
+		<div class="wp-block-buttons is-content-justification-left is-layout-flex wp-block-buttons-is-layout-flex print:hidden">
+			<div class="wp-block-button is-style-outline"><a href="mailto:info@beachfleischman.com?subject=Newsletter%20opt-in from:%20<?php echo esc_attr( get_the_title() ); ?>" class="wp-block-button__link has-brand-red-color has-text-color wp-element-button "><i class="fa-solid fa-envelope"></i> Email us</a></div>
+		</div>
+	</noscript>
 </div>
