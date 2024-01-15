b1b089293f0d58f5a9527872dddd4d6e0e3bbe38
Daniel Bishop <dbishop@beachfleischman.com>
2024-01-15 13:32:57 -0700

    Update content-card-ic.php

--- theme/template-parts/content/content-card-ic.php
+++ theme/template-parts/content/content-card-ic.php
@@ -14,7 +14,7 @@
 <li <?php post_class( 'card-ic | group flex flex-col relative border-neutral-100 border-2 shadow-orient-700 focus-within:shadow-lg focus-within:border-neutral-500 dark:shadow-orient-500' ); ?>>
 
 	<div class="card-text | flex flex-col text grow order-1 bg-white dark:bg-neutral-800 dark:text-neutral-300">
-		<?php the_title( '<h3 class="my-2 overflow-hidden tracking-wide break-words text-brand-blue dark:text-brand-blue-pale text-ellipsis"><a href="' . esc_url( get_permalink() ) . '" class="group-hover:decoration-brand-blue-pale focus:underline group-hover:underline">', '</a></h3>' ); ?>
+		<?php the_title( '<h3 class="my-2 overflow-hidden tracking-wide text-brand-blue dark:text-brand-blue-pale text-ellipsis"><a href="' . esc_url( get_permalink() ) . '" class="group-hover:decoration-brand-blue-pale focus:underline group-hover:underline">', '</a></h3>' ); ?>
 
 		<?php if ( has_excerpt() ) { ?>
 			<p class="mt-2 mb-4 text-sm lg:text-base"><?php echo get_the_excerpt(); ?></p>
