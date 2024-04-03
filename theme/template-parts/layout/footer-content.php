ddcfc5cfc1cc2d7874d7eda9e43a873ff0f40f38
Daniel Bishop <dbishop@beachfleischman.com>
2024-04-03 14:04:55 -0700

    Update footer-content.php
    Fixing anchor target for 'back to top' link

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -166,7 +166,7 @@ if ( !is_page_template( 'tpl-landing-page-bare.php' ) ) {
 				<?php } ?>
 
 				<p class="pt-4 pb-0 text-sm text-center uppercase print:hidden">
-					<a class="hover:text-brand-blue-faint" href="#top" title="Back to top"><i class="fa-regular fa-arrow-up-to-dotted-line"></i></a>
+					<a class="hover:text-brand-blue-faint" href="#page" title="Back to top"><i class="fa-regular fa-arrow-up-to-dotted-line"></i></a>
 				</p>
 			</div>
 		</div>
