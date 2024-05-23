cc1ca77a3f9787733ddfb587c68c3e5fc6200cef
Daniel Bishop <dbishop@beachfleischman.com>
2024-05-23 10:41:09 -0700

    Adding new starter list patterns to use in Bios

---
+++ theme/patterns/bio-industry-experience.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Title: Industry Experience
+ * Slug: loadlifter/industry-experience
+ * Categories: text
+ * Description: Starter list of all our industry pages, linked up
+ * Keywords:
+ * Block Types: core/list, core/heading
+ *
+ * @package loadlifter
+ * @since 1.4.0
+ */
+?>
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Industry Experience</h3>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/cannabis-phoenix-tucson-arizona-accountants-cpas/">Cannabis</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/construction-phoenix-tucson-arizona-accountants-cpas/">Construction</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/financial-professional-services-phoenix-tucson-arizona-accountants-cpas/">Financial &amp; Professional Services</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/healthcare-phoenix-tucson-arizona-accountants-cpas/">Healthcare</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/manufacturing-phoenix-tucson-arizona-accountants-cpas/">Manufacturing</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/nonprofit-phoenix-tucson-arizona-accountants-cpas/">Nonprofit</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/real-estate-phoenix-tucson-arizona-accountants-cpas/">Real Estate</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/restaurant-phoenix-tucson-arizona-accountants-cpas/">Restaurant</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://beachfleischman.com/industries/technology-phoenix-tucson-arizona-accountants-cpas/">Technology</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
