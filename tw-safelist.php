8ddf5dd334a1dc9a64156c0f7898ae3fb86e5725
Daniel Bishop <dbishop@beachfleischman.com>
2025-01-31 09:45:55 -0700

    Adding more classes to tw-safelist
    to fix logo on SOAR page

--- tw-safelist.php
+++ tw-safelist.php
@@ -3,7 +3,8 @@
  *
  */
 ?>
-<p class=" order-2 order-3 order-4 ">
+
+<p class=" order-2 order-3 order-4 w-72  |  md:float-right md:ml-2 lg:w-96 lg:ml-4 ">
 <!-- {
 			pattern: /text-(2|3|4|5|6|7|8|9)xl/,
 			variants: [ sm   md   lg ],
