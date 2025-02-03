bf0cccfd7fdbd1dc926ed19fb0b8640c9e8b992b
Daniel Bishop <dbishop@beachfleischman.com>
2025-02-03 16:48:10 -0700

    Updating awards shortcode
    Swapping in 2024 top 200 firms image

--- theme/inc/shortcodes.php
+++ theme/inc/shortcodes.php
@@ -125,7 +125,7 @@ function ll_awardlogos_shortcode() {
 			<img loading="lazy" src="https://res.cloudinary.com/beachfleischman/image/upload/c_scale,dpr_auto,f_auto,h_200,q_auto/v1717027280/2024_pbj_largest-phx-acct-firms_nyik4u.png" alt="2024 4th Largest Phoenix-Area Accounting Firms - Phoenix Business Journal" width="102" height="100">
 		</a>
 		<a href="https://insidepublicaccounting.com/" target="_blank" rel="noreferrer noopener">
-			<img loading="lazy" src="https://res.cloudinary.com/beachfleischman/image/upload/c_scale,dpr_auto,f_auto,h_200/v1691787197/IPA_-_Award_Logos_-_Top_200_Firms_nfmdem.png" alt="2023 Top 200 Firms - Inside Public Accounting" width="100" height="100">
+			<img loading="lazy" src="https://res.cloudinary.com/beachfleischman/image/upload/c_scale,dpr_auto,f_auto,h_200,q_auto/v1738626316/IPA_-_Award_Logo_-_Top_200_Firms_cvl1wy.png" alt="2024 Top 200 Firms - Inside Public Accounting" width="100" height="100">
 		</a>
 		<a href="https://bestcompaniesgroup.com/programs/inclusive-workplace-program/" target="_blank" rel="noreferrer noopener">
 			<img loading="lazy" src="https://res.cloudinary.com/beachfleischman/image/upload/c_scale,dpr_auto,f_auto,h_200/v1722882083/Inclusive_Workplace_July_24-July_25-c_pmft9t.png" alt="July 2024-July 2025 Inclusive Workplace - Best Companies Group" width="100" height="100">
