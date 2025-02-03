76a6b550920cf53206c1a5cec392605027079a75
Daniel Bishop <dbishop@beachfleischman.com>
2025-02-03 15:04:46 -0700

    Splitting css structure for better front and back loading

--- theme/functions.php
+++ theme/functions.php
@@ -13,7 +13,7 @@ if ( ! defined( 'LL_VERSION' ) ) {
 		*
 		* This is used primarily for cache busting. If you use `npm run bundle` to create your production build, the value below will be replaced in the generated zip file with a timestamp, converted to base 36.
 		*/
-	define( 'LL_VERSION', '3.0.4' );
+	define( 'LL_VERSION', '3.0.5' );
 }
 
 if ( ! defined( 'LL_COMPANY_LEGAL_NAME' ) ) {
