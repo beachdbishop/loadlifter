74b490eef608671d02690a3bc4a7b64e88468487
Daniel Bishop <dbishop@beachfleischman.com>
2025-05-01 16:58:16 -0700

    Update footer-simple.php

--- theme/template-parts/layout/footer-simple.php
+++ theme/template-parts/layout/footer-simple.php
@@ -26,7 +26,7 @@ if ( !is_page_template( 'tpl-landing-page-bare.php' ) ) {
 
 	<footer id="colophon" class="site-footer  |  wp-block-cover alignfull has-parallax border-t-4 border-solid border-orient-800 ll-equal-vert-padding !px-0 bg-neutral-950  |  print:border-none print:bg-white">
 		<span aria-hidden="true" class="wp-block-cover__background has-background-dim-70 has-background-dim print:hidden"></span>
-		<div role="img" aria-label="The Arizona desert at sunrise" class="wp-block-cover__image-background has-parallax  |  print:hidden" style="background-position:50% 0;background-image:url('<?php echo get_template_directory_uri(); ?>/img/phx-desert-color-no-crop.jpg')"></div>
+		<div role="img" aria-label="The Arizona desert at sunrise" class="deferred wp-block-cover__image-background has-parallax  |  print:hidden" style="background-position:50% 0;background-image:url('<?php echo get_template_directory_uri(); ?>/img/phx-desert-color-no-crop.jpg')"></div>
 
 		<div class="wp-block-cover__inner-container is-layout-flow wp-block-cover-is-layout-flow px-2 | lg:px-[16px]">
 			<div class="footer-grid  |  grid grid-cols-1 gap-x-8 gap-y-12 text-neutral-100 z-10  |  md:grid-cols-2 lg:grid-cols-5 print:hidden print:text-neutral-700">
